89575a70dba2e39b672b8f883f55bc4fd1c02daa
Džuris <juris@glaive.pro>
2023-04-30 01:34:22 +0300

    Document the wishlist

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -24,6 +24,21 @@ class DatabaseTest extends TestCase
 		$user = getenv('DB_USERNAME');
 		$pass = getenv('DB_PASSWORD');
 
+		/*
+			Driver name		Supported databases
+			PDO_CUBRID		Cubrid
+			PDO_DBLIB		FreeTDS / Microsoft SQL Server / Sybase
+			PDO_FIREBIRD	Firebird
+			PDO_IBM			IBM DB2
+			PDO_INFORMIX	IBM Informix Dynamic Server
+			PDO_MYSQL		MySQL 3.x/4.x/5.x/8.x
+			PDO_OCI			Oracle Call Interface
+			PDO_ODBC		ODBC v3 (IBM DB2, unixODBC and win32 ODBC)
+			PDO_PGSQL		PostgreSQL
+			PDO_SQLITE		SQLite 3 and SQLite 2
+			PDO_SQLSRV		Microsoft SQL Server / SQL Azure
+		 */
+
 		return [
 			'SQLite' => ['sqlite::memory:', null, null],
 			'MySQL' => ["mysql:host=mysql;port=3306;dbname=$db", $user, $pass],
