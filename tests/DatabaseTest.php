66485b9d519012621f8c41eaac48861b31818cc9
Džuris <juris@glaive.pro>
2023-04-29 17:52:41 +0300

    Use DataProvider

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -1,17 +1,27 @@
 <?php
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 class DatabaseTest extends TestCase
 {
-	public function testSqlite(): void
+	#[DataProvider('databases')]
+	public function testAdditionInDatabase(string $dsn, ?string $username, ?string $password): void
 	{
-		$pdo = new PDO('sqlite::memory:', null, null, [
+		$pdo = new PDO($dsn, $username, $password, [
 			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_COLUMN,
 		]);
+
 		$statement = $pdo->prepare('select 1+1');
 		$statement->execute();
 
 		$this->assertSame(2, $statement->fetch());
+	}
+
+	public static function databases():array
+	{
+		return [
+			'SQLite' => ['sqlite::memory:', null, null],
+		];
 	}
 }
